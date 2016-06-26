14e12f761e1c45a33866c61c14737a7b5a864a54
Jakub Juszczak <netghost03@gmail.com>
2016-06-26 15:34:15 +0200

    ✨ Add new path to webpack config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -18,7 +18,8 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'BaseCharts': path.resolve(__dirname, '../src/BaseCharts')
     }
   },
   resolveLoader: {
