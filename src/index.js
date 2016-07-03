a428b517416d5f9e5e97c02d9e51e03cbcc7c369
Jakub Juszczak <netghost03@gmail.com>
2016-07-03 12:06:58 +0200

    Add index file for npm package

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Bar from './BaseCharts/Bar'
+import Doughnut from './BaseCharts/Doughnut'
+import Line from './BaseCharts/Line'
+import Pie from './BaseCharts/Pie'
+import PolarArea from './BaseCharts/PolarArea'
+import Radar from './BaseCharts/Radar'
+
+const VueCharts = {
+  Bar,
+  Doughnut,
+  Line,
+  Pie,
+  PolarArea,
+  Radar
+}
+
+module.exports = VueCharts
