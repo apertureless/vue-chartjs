99f1d97dad378af33f084ea54f72bebf29ed6c5e
Jakub Juszczak <netghost03@gmail.com>
2016-08-21 13:56:13 +0200

    🔥 Remove testing code

--- src/index.js
+++ src/index.js
@@ -1,49 +1,17 @@
-// import Bar from './BaseCharts/Bar'
-// import Doughnut from './BaseCharts/Doughnut'
-// import Line from './BaseCharts/Line'
-// import Pie from './BaseCharts/Pie'
-// import PolarArea from './BaseCharts/PolarArea'
-// import Radar from './BaseCharts/Radar'
+import Bar from './BaseCharts/Bar'
+import Doughnut from './BaseCharts/Doughnut'
+import Line from './BaseCharts/Line'
+import Pie from './BaseCharts/Pie'
+import PolarArea from './BaseCharts/PolarArea'
+import Radar from './BaseCharts/Radar'
 
-// const VueCharts = {
-//   Bar,
-//   Doughnut,
-//   Line,
-//   Pie,
-//   PolarArea,
-//   Radar
-// }
-
-// module.exports = VueCharts
-
-import Vue from 'vue'
-import App from './App'
-import { optionExtend } from './helpers/options'
-
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
-
-let defaultObj = {
-  options: {
-    scale: {
-      x: 12,
-      y: 13
-    }
-  }
+const VueCharts = {
+  Bar,
+  Doughnut,
+  Line,
+  Pie,
+  PolarArea,
+  Radar
 }
 
-let newObj = {
-  options: {
-    scale: {
-      x: 66
-    }
-  }
-}
-
-let mergedObj = optionExtend(defaultObj, newObj)
-let testMerge = Object.assign(defaultObj, newObj)
-console.log(mergedObj)
-console.log(testMerge)
+module.exports = VueCharts
