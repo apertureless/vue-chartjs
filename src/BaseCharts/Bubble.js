3785e73b4335c42b202541480f99616f4f4aa881
Juan Carlos Alonso <me@jcalonso.com>
2016-09-07 22:50:18 +0100

    Added bubble chart type

---
+++ src/BaseCharts/Bubble.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Chart from 'chart.js'
+import { mergeOptions } from '../helpers/options'
+
+export default Vue.extend({
+  template: `
+    <div>
+      <canvas id="{{chartId}}" width={{width}} height={{height}} v-el:canvas></canvas>
+    </div>
+  `,
+
+  props: {
+    chartId: {
+      default: 'bubble-chart',
+      type: String
+    },
+    width: {
+      default: 400,
+      type: Number
+    },
+    height: {
+      default: 400,
+      type: Number
+    }
+  },
+
+  data () {
+    return {
+      defaultOptions: {
+      }
+    }
+  },
+
+  methods: {
+    render (data, options) {
+      let chartOptions = mergeOptions(this.defaultOptions, options)
+
+      this._chart = new Chart(
+        this.$els.canvas.getContext('2d'), {
+          type: 'bubble',
+          data: data,
+          options: chartOptions
+        }
+      )
+      this._chart.generateLegend()
+    }
+  },
+  beforeDestroy () {
+    this._chart.destroy()
+  }
+})
