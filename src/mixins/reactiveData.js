f94b320dfa48587a10cec4b685ab2e58cb20c343
Jakub Juszczak <netghost03@gmail.com>
2016-10-15 12:58:51 +0200

    ✨ Change reactiveDataMixin

--- src/mixins/reactiveData.js
+++ src/mixins/reactiveData.js
@@ -19,16 +19,26 @@ module.exports = {
           })
 
           if (JSON.stringify(newDataLabels) === JSON.stringify(oldDataLabels)) {
-            newData.datasets.forEach((dataset, i) => {
-              chart.data.datasets[i].data = dataset.data
-            })
-            chart.data.labels = newData.labels
-            chart.update()
+            this.forceUpdate(newData, chart)
           } else {
-            this.renderChart(this.chartData, this.options)
+            this.forceRender()
           }
         }
       }
+    }
+  },
+  methods: {
+    forceUpdate (newData, chart) {
+      newData.datasets.forEach((dataset, i) => {
+        chart.data.datasets[i].data = dataset.data
+      })
+
+      chart.data.labels = newData.labels
+      chart.update()
+    },
+
+    forceRender () {
+      this.renderChart(this.chartData, this.options)
     }
   }
 }
