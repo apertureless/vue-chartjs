6ef85af85a16421c13ba7ad6828c71e7910656e2
Jakub Juszczak <netghost03@gmail.com>
2017-02-27 22:48:23 +0100

    ✨ Add docs landingpage

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,11 @@
 self.$config = {
-  // config...
+  plugins: [
+    evanyou()
+  ],
+  landing: true,
+  // or custom path
+  landing: '_landing.html',
+  repo: 'apertureless/vue-chartjs',
+  twitter: 'apertureless',
+  'edit-link': 'https://github.com/apertureless/vue-chartjs/blob/master/docs'
 }
