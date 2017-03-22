348de825bfb6a14a68fb551cd18fbead0cc0ff48
Jakub Juszczak <netghost03@gmail.com>
2017-03-22 19:02:49 +0100

    ✨ Add new entry for development

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,7 +5,7 @@ var projectRoot = path.resolve(__dirname, '../')
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
