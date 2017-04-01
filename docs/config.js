77c63d4dd4af28ed048880099b83302af273e96a
Jakub Juszczak <netghost03@gmail.com>
2017-04-01 19:59:25 +0200

    ⬆️ Update docute docs

--- docs/config.js
+++ docs/config.js
@@ -1,12 +1,22 @@
-self.$config = {
+docute.init({
   title: 'vue-chartjs docs',
   plugins: [
     evanyou()
   ],
   landing: true,
-  // or custom path
   landing: '_landing.html',
   repo: 'apertureless/vue-chartjs',
   twitter: 'apertureless',
-  'edit-link': 'https://github.com/apertureless/vue-chartjs/blob/master/docs'
-}
+  tocVisibleDepth: 2,
+  'edit-link': 'https://github.com/apertureless/vue-chartjs/blob/master/docs',
+  nav: {
+    default: [
+      {
+        title: 'Home', path: '/home'
+      },
+      {
+        title: 'Changelog', path: '/changelog', source: 'https://raw.githubusercontent.com/apertureless/vue-chartjs/develop/CHANGELOG.md'
+      },
+    ]
+  }
+})
