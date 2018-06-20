23b9a3fb8a2996e857d1be2670f3ce6150339536
Jakub Juszczak <netghost03@gmail.com>
2018-06-20 17:35:53 +0200

    feat(core): Add basic type definitions

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+import { BaseChart } from './components'
+
+declare module 'vue-chartjs' {
+  export function generateChart(chartId: string, chartType: string): any;
+  export class Bar extends BaseChart {}
+  export class HorizontalBar extends BaseChart {}
+  export class Doughnut extends BaseChart {}
+  export class Line extends BaseChart {}
+  export class Pie extends BaseChart {}
+  export class PolarArea extends BaseChart {}
+  export class Radar extends BaseChart {}
+  export class Bubble extends BaseChart {}
+  export class Scatter extends BaseChart {}
+}
