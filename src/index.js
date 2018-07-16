23ff90d6334987ecff65539309acb93652f665d0
Jakub Juszczak <netghost03@gmail.com>
2018-07-16 12:22:59 +0200

    feat(ux): Add fake render method with error message
    Closes #380

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ const VueCharts = {
   Bubble,
   Scatter,
   mixins,
-  generateChart
+  generateChart,
+  render: () => console.error('[vue-chartjs]: This is not a vue component. It is the whole object containing all vue components. Please import the named export or access the components over the dot notation. For more info visit https://vue-chartjs.org/#/home?id=quick-start')
 }
 
 export default VueCharts
