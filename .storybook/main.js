05bbdd381566c5e95339d8e1498216b2ff4e6f6c
v.terekhov <v.terekhov@iportal.ru>
2022-01-17 18:56:20 +0400

    feat: add theme config for storybook and remove not used addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   stories: ['../stories/*.stories.(js|ts)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials'],
   framework: '@storybook/vue',
   webpackFinal: async config => {
     config.resolve.alias['vue-chartjs'] = path.resolve(
---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming'
+
+export const theme = create({
+  base: 'light',
+  brandTitle: 'vue-chartjs',
+  brandUrl: 'https://github.com/apertureless/vue-chartjs'
+})
