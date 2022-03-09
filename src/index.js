4ebae7285b2401c926efec05cb07f9fb2f3d7e4d
v.terekhov <v.terekhov@iportal.ru>
2022-03-09 16:30:59 +0400

    fix: remove vueCharts export and change module for size-limit tests

--- src/index.js
+++ src/index.js
@@ -10,26 +10,7 @@ import {
   generateChart
 } from './BaseCharts'
 
-const VueCharts = {
-  Bar,
-  Doughnut,
-  Line,
-  Pie,
-  PolarArea,
-  Radar,
-  Bubble,
-  Scatter,
-  generateChart,
-  render: () =>
-    console.error(
-      '[vue-chartjs]: This is not a vue component. It is the whole object containing all vue components. Please import the named export or access the components over the dot notation. For more info visit https://vue-chartjs.org/#/home?id=quick-start'
-    )
-}
-
-export default VueCharts
-
 export {
-  VueCharts,
   Bar,
   Doughnut,
   Line,
