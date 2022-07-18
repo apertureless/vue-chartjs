bad502638dd70837018969075eb1e5f378498167
Slava Terekhov <thabarbados@gmail.com>
2022-07-18 14:36:30 +0400

    chore(deps): update dev dependencies and tune renovate config (#891)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  stories: ['../stories/*.stories.(js|ts)'],
+  stories: ['../stories/*.stories.@(ts|js)'],
   addons: ['@storybook/addon-essentials'],
   framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
   webpackFinal: async config => {
     config.resolve.alias['vue-chartjs'] = path.resolve(
       __dirname,
